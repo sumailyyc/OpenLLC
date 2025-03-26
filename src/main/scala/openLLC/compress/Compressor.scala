3c3e90ec9cc77c95e97687bcc25d40231780c376
sumailyyc <810583334@qq.com>
2025-03-26 21:45:21 +0800

    Implement DGB Compressor/Decompressor

--- src/main/scala/openLLC/compress/Compressor.scala
+++ src/main/scala/openLLC/compress/Compressor.scala
@@ -20,6 +20,8 @@ package openLLC.compress
 import chisel3._
 import chisel3.util._
 import org.chipsalliance.cde.config.Parameters
+import DataTransformer._
+import utility.{ParallelMax, ParallelMin}
 
 /** IO bundle definition for an Compressor, which takes one valid input and outputs
   * the compressed data.
@@ -54,11 +56,11 @@ abstract class Compressor[T <: Data](val gen: T) extends Module {
 
 class ZeroValueCompressor(gen: UInt, val itemSize: Int) extends Compressor[UInt](gen) {
 
-  assert(isPow2(itemSize), "Illegal item size")
+  require(isPow2(itemSize), "Illegal item size")
 
   def compress(rawData: Valid[UInt]): (Valid[UInt], Bool, UInt) = {
     val dataWidth = rawData.bits.getWidth
-    assert(dataWidth % itemSize == 0, "Illegal data length")
+    require(dataWidth % itemSize == 0, "Illegal data length")
 
     val numItems = dataWidth / itemSize
     val itemVec = Wire(Vec(numItems, UInt(itemSize.W)))
@@ -82,6 +84,7 @@ class ZeroValueCompressor(gen: UInt, val itemSize: Int) extends Compressor[UInt]
     }.otherwise {
       compressedData.bits := rawData.bits
     }
+    assert(canCompress || rawData.bits === compressedData.bits)
     (compressedData, canCompress, totalLen)
   }
 }
@@ -92,37 +95,210 @@ class DontCompressor(gen: UInt) extends Compressor[UInt](gen) {
   }
 }
 
-/* class DGBCompressor(val gen: DSBlock, val n: Int) extends Compressor[DSBlock](gen, n) {
-  private def DGB(rawData: UInt, itemSize: Int): UInt = {
-    assert(rawData.getWidth % itemSize == 0, "Illegal data length")
-    val numItems = rawData.getWidth / itemSize
+object DataTransformer {
+
+  def deltaEncoding(data: UInt, itemSize: Int): UInt = {
+    require(isPow2(itemSize), "Illegal item size")
+    require(data.getWidth % itemSize == 0, "Illegal data length")
+    val numItems = data.getWidth / itemSize
     val itemVec = Wire(Vec(numItems, UInt(itemSize.W)))
     itemVec.zipWithIndex.foreach { case (e, i) =>
-      e :=  rawData((i + 1) * itemSize - 1, i * itemSize)
+      e :=  data((i + 1) * itemSize - 1, i * itemSize)
     }
-    def deltaEncoding(dataVec: Vec[UInt]): Vec[UInt] = {
-      val encodingVec = Wire(chiselTypeOf(dataVec))
-      val baseValue = dataVec.head
-      encodingVec.head := baseValue
-      encodingVec.tail.zip.dataVec.tail.foreach { case (sink, src) =>
-        sink := src - baseValue
-      }
-      encodingVec
+    val encodingVec = Wire(chiselTypeOf(itemVec))
+    val baseValue = itemVec.head
+    encodingVec.head := baseValue
+    (encodingVec.tail zip itemVec.tail).foreach { case (sink, src) =>
+      sink := src - baseValue
     }
-    def grayEncoding(data: UInt): UInt = data ^ (data >> 1.U)
-    def bitPlaneEncoding(dataVec: Vec[UInt]): Vec[UInt] = {
-      val rows = dataVec.size
-      val cols = dataVec.head.getWidth
-      val encodingVec = Wire(Vec(cols, UInt(rows.W)))
-      encodingVec.zipWithIndex.foreach { case (e, i) =>
-        e := Cat(dataVec.map(t => t(i)))
-      }
-      encodingVec
-    }
-    bitPlaneEncoding(VecInit(deltaEncoding(itemVec).map(_ => grayEncoding(_))))
+    encodingVec.asUInt
   }
-  
-  def compress(in: Valid[DSBlock]) = {
 
+  def grayEncoding(data: UInt): UInt = data ^ (data >> 1.U)
+
+  def bitPlaneEncoding(data: UInt, itemSize: Int): UInt = {
+    val totalWidth = data.getWidth
+
+    require(isPow2(itemSize), "Illegal item size")
+    require(totalWidth % itemSize == 0, "Data width must be divisible by itemSize")
+
+    val rows = totalWidth / itemSize
+    val cols = itemSize
+
+    val encodedBits = Seq.tabulate(cols) { col =>
+      Seq.tabulate(rows) { row =>
+        val bitPos = (rows - 1 - row) * cols + (cols - 1 - col)
+        data(bitPos)
+      }
+    }.flatten
+
+    Cat(encodedBits)
   }
-} */
+
+  def deltaGrayBitPlaneEncoding(data: UInt, itemSize: Int): (UInt, UInt) = {
+    require(isPow2(itemSize), "Illegal item size")
+    require(data.getWidth % itemSize == 0, "Illegal data length")
+    val deltaCode = deltaEncoding(data, itemSize)
+    val numItems = deltaCode.getWidth / itemSize
+    val itemVec = Wire(Vec(numItems, UInt(itemSize.W)))
+    itemVec.zipWithIndex.foreach { case (e, i) =>
+      e :=  deltaCode((i + 1) * itemSize - 1, i * itemSize)
+    }
+    val grayCode = VecInit(itemVec.map(grayEncoding(_))).asUInt
+    (bitPlaneEncoding(grayCode, itemSize), grayCode)
+  }
+
+  def deltaDecoding(data: UInt, itemSize: Int): UInt = {
+    require(isPow2(itemSize), "Illegal item size")
+    require(data.getWidth % itemSize == 0, "Illegal data length")
+    val numItems = data.getWidth / itemSize
+    val itemVec = Wire(Vec(numItems, UInt(itemSize.W)))
+    itemVec.zipWithIndex.foreach { case (e, i) =>
+      e :=  data((i + 1) * itemSize - 1, i * itemSize)
+    }
+    val encodingVec = Wire(chiselTypeOf(itemVec))
+    val baseValue = itemVec.head
+    encodingVec.head := baseValue
+    (encodingVec.tail zip itemVec.tail).foreach { case (sink, src) =>
+      sink := src + baseValue
+    }
+    encodingVec.asUInt
+  }
+
+  def grayDecoding(data: UInt): UInt = {
+    val width = data.getWidth
+    val binary = Wire(Vec(width, Bool()))
+    binary(width - 1) := data(width - 1)
+    for (i <- (width-2) to 0 by -1) {
+      binary(i) := data(i) ^ binary(i + 1)
+    }
+    binary.asUInt 
+  }
+
+  def bitPlaneDecoding(data: UInt, itemSize: Int): UInt = {
+    val totalWidth = data.getWidth
+
+    require(isPow2(itemSize), "Illegal item size")
+    require(totalWidth % itemSize == 0, "Data width must be divisible by itemSize")
+
+    val rows = totalWidth / itemSize
+    val cols = itemSize
+
+    val decodedBits = Seq.tabulate(rows) { row =>
+      Seq.tabulate(cols) { col =>
+        val bitPos = (cols - 1 - col) * rows + (rows - 1 - row)
+        data(bitPos)
+      }
+    }.flatten
+
+    Cat(decodedBits)
+  }
+
+  def deltaGrayBitPlaneDecoding(data: UInt, itemSize: Int, bitPlaneEnable: Bool): UInt = {
+    require(isPow2(itemSize), "Illegal item size")
+    require(data.getWidth % itemSize == 0, "Illegal data length")
+    val bitPlaneDecode = bitPlaneDecoding(data, itemSize)
+    val numItems = bitPlaneDecode.getWidth / itemSize
+    val itemVec = Wire(Vec(numItems, UInt(itemSize.W)))
+    itemVec.zipWithIndex.foreach { case (e, i) =>
+      e :=  Mux(
+        bitPlaneEnable,
+        bitPlaneDecode((i + 1) * itemSize - 1, i * itemSize),
+        data((i + 1) * itemSize - 1, i * itemSize)
+      )
+    }
+    deltaDecoding(VecInit(itemVec.map(grayDecoding(_))).asUInt, itemSize)
+  }
+}
+
+class DGBCompressor(gen: UInt) extends Compressor[UInt](gen) {
+  
+  def compress(in: Valid[UInt]) = {
+    val rawData = in.bits
+    val dgb_8b = deltaGrayBitPlaneEncoding(rawData, 8)
+    val dgb_16b = deltaGrayBitPlaneEncoding(rawData, 16)
+    val dgb_32b = deltaGrayBitPlaneEncoding(rawData, 32)
+    val dgb_64b = deltaGrayBitPlaneEncoding(rawData, 64)
+
+    val encodeVec = Wire(Vec(9, chiselTypeOf(rawData)))
+    encodeVec(0) := rawData
+    encodeVec(1) := dgb_8b._1
+    encodeVec(2) := dgb_8b._2
+    encodeVec(3) := dgb_16b._1
+    encodeVec(4) := dgb_16b._2
+    encodeVec(5) := dgb_32b._1
+    encodeVec(6) := dgb_32b._2
+    encodeVec(7) := dgb_64b._1
+    encodeVec(8) := dgb_64b._2
+
+    val zeroItemCountVec = VecInit(encodeVec.map { e =>
+      val numItems = e.getWidth / 8
+      val vec = Wire(Vec(numItems, UInt(8.W)))
+      for (i <- 0 until numItems) { vec(i) := e((i + 1) * 8 - 1, i * 8) }
+      PopCount(vec.map(_ === 0.U))
+    })
+    val zeroItemCountMax = ParallelMax(zeroItemCountVec)
+    val encodeId = PriorityEncoder(VecInit(zeroItemCountVec.map(_ === zeroItemCountMax)))
+    val toZVC = encodeVec(encodeId)
+    val dgbId = MuxLookup(encodeId, 0.U)(Seq(
+      0.U -> 0.U,
+      1.U -> 1.U,
+      2.U -> 1.U,
+      3.U -> 2.U,
+      4.U -> 2.U,
+      5.U -> 3.U,
+      6.U -> 3.U,
+      7.U -> 4.U,
+      8.U -> 4.U,
+    ))
+    val bitPlaneEnable = encodeId(0)
+
+    val zvc_8b = Module(new ZeroValueCompressor(rawData, 8))
+    val zvc_16b = Module(new ZeroValueCompressor(rawData, 16))
+    val zvc_32b = Module(new ZeroValueCompressor(rawData, 32))
+    val zvc_64b = Module(new ZeroValueCompressor(rawData, 64))
+
+    zvc_8b.io.in.valid := in.valid
+    zvc_16b.io.in.valid := in.valid
+    zvc_32b.io.in.valid := in.valid
+    zvc_64b.io.in.valid := in.valid
+    zvc_8b.io.in.bits := toZVC
+    zvc_16b.io.in.bits := toZVC
+    zvc_32b.io.in.bits := toZVC
+    zvc_64b.io.in.bits := toZVC
+
+    val dataVec = Wire(Vec(4, chiselTypeOf(zvc_8b.io.out.bits)))
+    val compressedVec = Wire(Vec(4, chiselTypeOf(zvc_8b.io.compressed)))
+    val lengthVec = Wire(Vec(4, chiselTypeOf(zvc_8b.io.length)))
+
+    dataVec(0) := zvc_8b.io.out.bits
+    dataVec(1) := zvc_16b.io.out.bits
+    dataVec(2) := zvc_32b.io.out.bits
+    dataVec(3) := zvc_64b.io.out.bits
+    compressedVec(0) := zvc_8b.io.compressed
+    compressedVec(1) := zvc_16b.io.compressed
+    compressedVec(2) := zvc_32b.io.compressed
+    compressedVec(3) := zvc_64b.io.compressed
+    lengthVec(0) := Mux(zvc_8b.io.compressed, zvc_8b.io.length, (zvc_8b.io.out.bits.getWidth - 1).U)
+    lengthVec(1) := Mux(zvc_16b.io.compressed, zvc_16b.io.length, (zvc_16b.io.out.bits.getWidth - 1).U)
+    lengthVec(2) := Mux(zvc_32b.io.compressed, zvc_32b.io.length, (zvc_32b.io.out.bits.getWidth - 1).U)
+    lengthVec(3) := Mux(zvc_64b.io.compressed, zvc_64b.io.length, (zvc_64b.io.out.bits.getWidth - 1).U)
+    assert(zvc_8b.io.compressed || lengthVec(0) === (zvc_8b.io.out.bits.getWidth - 1).U)
+
+    val lengthMin = ParallelMin(lengthVec)
+    val dataId = PriorityEncoder(VecInit(lengthVec.map(_ === lengthMin)))
+    val dataSelected = dataVec(dataId)
+
+    val zvcId = dataId
+    val header = Cat(zvcId, bitPlaneEnable, dgbId)
+    val combined = Cat(header, dataSelected)
+    val totalLen = header.getWidth.U +& lengthVec(zvcId)
+    val canCompress = compressedVec.asUInt.orR && (totalLen < rawData.getWidth.U)
+
+    val compressedData = Wire(chiselTypeOf(in))
+    compressedData.valid := in.valid
+    compressedData.bits := Mux(canCompress, combined(combined.getWidth - 1, header.getWidth), rawData)
+    assert(canCompress || rawData === compressedData.bits)
+    (compressedData, canCompress, totalLen)
+  }
+}
--- src/main/scala/openLLC/compress/Decompressor.scala
+++ src/main/scala/openLLC/compress/Decompressor.scala
@@ -20,6 +20,7 @@ package openLLC.compress
 import chisel3._
 import chisel3.util._
 import org.chipsalliance.cde.config.Parameters
+import DataTransformer._
 
 class DecompressorIO[T <: Data](private val gen: T) extends Bundle {
   // Compressed input data
@@ -68,5 +69,51 @@ class ZeroValueDecompressor(gen: UInt, val itemSize: Int) extends Decompressor[U
 class DontDecompressor(gen: UInt) extends Decompressor[UInt](gen) {
   def decompress(compressedData: Valid[UInt]): Valid[UInt] = {
     compressedData
+  }
+}
+
+class DGBDecompressor(gen: UInt) extends Decompressor[UInt](gen) {
+  def decompress(compressedData: Valid[UInt]): Valid[UInt] = {
+    val dataWidth = compressedData.bits.getWidth
+
+    val combined = compressedData.bits ## 0.U(6.W)
+    val header = combined(combined.getWidth - 1, dataWidth)
+    val data = combined(dataWidth - 1, 0)
+    val dgbId = header(2, 0)
+    val bitPlaneEnable = header(3)
+    val zvcId = header(5, 4)
+
+    val zvcd_8b = Module(new ZeroValueDecompressor(data, 8))
+    val zvcd_16b = Module(new ZeroValueDecompressor(data, 16))
+    val zvcd_32b = Module(new ZeroValueDecompressor(data, 32))
+    val zvcd_64b = Module(new ZeroValueDecompressor(data, 64))
+
+    zvcd_8b.io.in.valid := compressedData.valid
+    zvcd_16b.io.in.valid := compressedData.valid
+    zvcd_32b.io.in.valid := compressedData.valid
+    zvcd_64b.io.in.valid := compressedData.valid
+    zvcd_8b.io.in.bits := data
+    zvcd_16b.io.in.bits := data
+    zvcd_32b.io.in.bits := data
+    zvcd_64b.io.in.bits := data
+
+    val zvcDecodeVec = Wire(Vec(4, chiselTypeOf(data)))
+    zvcDecodeVec(0) := zvcd_8b.io.out.bits
+    zvcDecodeVec(1) := zvcd_16b.io.out.bits
+    zvcDecodeVec(2) := zvcd_32b.io.out.bits
+    zvcDecodeVec(3) := zvcd_64b.io.out.bits
+
+    val zvcDecode = zvcDecodeVec(zvcId)
+    val dgbDecodeVec = Wire(Vec(5, chiselTypeOf(zvcDecode)))
+    dgbDecodeVec(0) := zvcDecode
+    dgbDecodeVec(1) := deltaGrayBitPlaneDecoding(zvcDecode, 8, bitPlaneEnable)
+    dgbDecodeVec(2) := deltaGrayBitPlaneDecoding(zvcDecode, 16, bitPlaneEnable)
+    dgbDecodeVec(3) := deltaGrayBitPlaneDecoding(zvcDecode, 32, bitPlaneEnable)
+    dgbDecodeVec(4) := deltaGrayBitPlaneDecoding(zvcDecode, 64, bitPlaneEnable)
+
+    val uncompressedData = Wire(chiselTypeOf(compressedData))
+    uncompressedData.valid := compressedData.valid
+    uncompressedData.bits := dgbDecodeVec(dgbId)
+    uncompressedData
   }
 }
